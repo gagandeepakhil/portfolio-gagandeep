327fe8b870d7a64363c1ab7995be0b36a07aa370
GAGAN DEEP DUNNA <103984604+gagandeepakhil@users.noreply.github.com>
2024-11-27 02:40:04 +0530

    added ui-rbac project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,28 +1,49 @@
 import React from "react";
 import pro from "../projects";
 
-function Project(){
-    return(
-        <>
-            <h1>🌟LATEST BUILDS🌟</h1>
-            <div id="projects">
-            {pro.map(pr=>{
-                return(
-                    <div className="pro">
-                        <img src={pr.img} alt={pro.title} />
-                        <h4>{pr.title}</h4>
-                        <p>{pr.text}</p>
-                        <div className="link">
-                            <a href={pr.git} target="_blank"><img src="images/github-btn.png" alt={pr.git} /></a>
-                            <a href={pr.demo} target="_blank"><button>LIVE DEMO</button></a>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
-        </>
-        
-    )
+function Project() {
+  return (
+    <>
+      <h1>🌟LATEST BUILDS🌟</h1>
+      <div id="projects">
+        {pro.map((pr) => {
+          return (
+            <div className="pro">
+              <img src={pr.img} alt={pro.title} />
+              <h4>{pr.title}</h4>
+              <p>{pr.text}</p>
+              <div className="link">
+                <a
+                  href={pr.git}
+                  onClick={(e) => {
+                    if (pr?.gitDsiabled) {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={pr?.gitDsiabled ? "disabled" : ""}
+                  target="_blank"
+                >
+                  <img src="images/github-btn.png" alt={pr.git} />
+                </a>
+                <a
+                  href={pr.demo}
+                  onClick={(e) => {
+                    if (pr?.demoDisabled) {
+                      e.preventDefault();
+                    }
+                  }}
+                  className={pr?.demoDisabled ? "disabled" : ""}
+                  target="_blank"
+                >
+                  <button>LIVE DEMO</button>
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
 export default Project;
--- src/projects.js
+++ src/projects.js
@@ -1,73 +1,102 @@
 const pro=[
     {
+        img:"images/rbac-ui.png",
+        title:"RBAC-UI",
+        text:"A web app for role based access control(RBAC) built using REACT JS and material UI with node js backend.",
+        git:"https://github.com/gagandeepakhil/rvr_security_frontend",
+        demo:"https://ui-rbac.netlify.app/",
+        gitDsiabled:false,
+        demoDisabled:false
+    },
+    {
         img:"images/data-anlysis(aqi).png",
         title:"DATA ANALYSIS(AQI OF VSKP)",
         text:"A simple course project data anlysis of AQI of Visakhaptnam city from June 2022 to May 2023 using python.",
         git:"https://github.com/gagandeepakhil/data-analysis-AQI-",
-        demo:"https://gagandeepakhil.github.io/data-analysis-AQI-/"
+        demo:"https://gagandeepakhil.github.io/data-analysis-AQI-/",
+        gitDasbled:false,
+        demoDisabled:false
     },
     {
         img:"images/weather-app-angular.png",
         title:"WEATHER APP (ANGULAR)",
         text:"Live weather app that tells the temperature of a city made by using Angular JS and open weather API.",
         git:"https://github.com/gagandeepakhil/weather_app_angular",
-        demo:"https://angular-weather-alclove.netlify.app/"
+        demo:"https://angular-weather-alclove.netlify.app/",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/weather-app-react.png",
         title:"WEATHER APP (REACT)",
         text:"Live weather app that tells the temperature of a city made by using Angular JS and open weather API.",
         git:"https://github.com/gagandeepakhil/weather-app",
-        demo:"https://gweatherapps.netlify.app/"
+        demo:"https://gweatherapps.netlify.app/",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/portfolio.png",
         title:"PORTFOLIO",
         text:"An animated portfolio website built using REACT JS.Learned to implement the react framework,routing in react and hosting a react webpage...",
         git:"https://github.com/gagandeepakhil/portfolio-gagandeep",
-        demo:"https://www.gagandeepdunna.netlify.app/"
+        demo:"https://www.gagandeepdunna.netlify.app/",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/itelugu.png",
         title:"ITELUGU",
         text:"A web app for international telugu conference built using EXPRESS JS ,MONGO DB .Learnt managing databases for different categories of data and much more...",
         git:"https://github.com/gagandeepakhil/itelugu",
-        demo:"https://www.itelugu.in/"
+        demo:"https://www.itelugu.in/",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/a-hub.png",
         title:"A-HUB",
         text:"A web app for AU incubation center(A-HUB) built using simple frontend framework (bootstrap).Learnt team management skills...",
         git:"https://github.com/horusknox/A-hub",
-        demo:"https://www.a-hub.in"
+        demo:"https://www.a-hub.in",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/avenue.png",
         title:"AVENUE-SALON",
         text:"A web app for salon named AVENUE built using frontend frameworks.Implemented many types of animations.Learnt advanced CSS animations using JS...",
         git:"https://github.com/nallamdeepak/avenue",
-        demo:"https://nallamdeepak.github.io/avenue"
+        demo:"https://nallamdeepak.github.io/avenue",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/myblog.png",
         title:"BLOG",
         text:"A personal blog project built using EXPRESS JS,MONGO DB.Learnt ejs rendering, implementing mongodb in live websites, deploying express apps using MVC model...",
         git:"https://github.com/gagandeepakhil/myblog",
-        demo:"https://myblog-taupe-beta.vercel.app/"
+        demo:"https://myblog-taupe-beta.vercel.app/",
+        gitDsiabled:true,
+        demoDisabled:true
     },
     {
         img:"images/drumkit.png",
         title:"DRUM KIT",
         text:"A frontend project using JS which plays sounds on a click or button press.Learnt adding event listeners to website and playing sounds in a website...",
         git:"https://github.com/gagandeepakhil/drumkit",
-        demo:"https://gagandeepakhil.github.io/drumkit"
+        demo:"https://gagandeepakhil.github.io/drumkit",
+        gitDsiabled:false,
+        demoDisabled:false
     },
     {
         img:"images/basic-calculator.png",
         title:"BASIC CALCULATOR",
-        text:"A basic calculator that can perform basic arithmetic operations on integers.Learnt writing javascript code...",
+        text:"A basic calculator that can perform basic arithmetic operations on integers.Uses postfix conversion, postfix evaluation logic.Learnt writing javascript code...",
         git:"https://github.com/gagandeepakhil/basic-calculator",
-        demo:"https://gagandeepakhil.github.io/basic-calculator"
+        demo:"https://gagandeepakhil.github.io/basic-calculator",
+        gitDsiabled:false,
+        demoDisabled:false
     },
 ]
 
